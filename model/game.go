1a2ccca0b7385e4b1acfe0a025b3db42043c79b2
Sam Delacruz <samdelacruz91@gmail.com>
2017-05-15 22:37:36 +0100

    Game score should be zero when no moves have been made

--- model/game.go
+++ model/game.go
@@ -1,16 +1,25 @@
 package model
 
+// Game stores the current state of a game
+// All properties can be derived from this struct
+// i.e score, win/lose, visible tiles, hints
 type Game struct {
 	ID    string
 	Board [25]byte
 	Moves []int
 }
 
+// Hint stores the hint metadata for a row/column
+// Mines is the number of mines/zeroes in a row/column
+// Points is the sum of a row/column
 type Hint struct {
 	Mines  int
 	Points int
 }
 
+// GetVisible gets the string representation of a gameboard
+// Unrevealed tiles represented as '?'
+// Revealed tiles represented by their points value
 func (g *Game) GetVisible() string {
 	v := []byte{
 		'?', '?', '?', '?', '?',
@@ -26,7 +35,13 @@ func (g *Game) GetVisible() string {
 	return string(v)
 }
 
+// GetScore returns the current score for a game
+// Score is multiplicative - the product of all revealed tiles
+// Edge case: when no tiles have been revealed, score is zero.
 func (g *Game) GetScore() int {
+	if len(g.Moves) == 0 {
+		return 0
+	}
 	s := 1
 	for _, m := range g.Moves {
 		s *= int(g.Board[m])
