22d58524608584a25e0d7dfb510e04971ce37d95
Sam Delacruz <samdelacruz91@gmail.com>
2017-05-16 20:38:29 +0100

    Add methods to Game for hints & score

--- model/game.go
+++ model/game.go
@@ -17,6 +17,13 @@ type Hint struct {
 	Points int
 }
 
+// Hints is the collection type for grouping hints
+// by rows and columns
+type Hints struct {
+	Rows [5]Hint
+	Cols [5]Hint
+}
+
 // GetVisible gets the string representation of a gameboard
 // Unrevealed tiles represented as '?'
 // Revealed tiles represented by their points value
@@ -47,4 +54,53 @@ func (g *Game) GetScore() int {
 		s *= int(g.Board[m])
 	}
 	return s
+}
+
+// GetMaxScore returns the maximum number of points available
+// for the given game board
+func (g *Game) GetMaxScore() int {
+	t := 0
+	for _, v := range g.Board {
+		if v > 0 {
+			if t == 0 {
+				t++
+			}
+			t *= int(v)
+		}
+	}
+	return t
+}
+
+// GetHints returns the set of hints for a game
+func (g *Game) GetHints() Hints {
+	var h Hints
+	for i := 0; i < 5; i++ {
+		h.Cols[i] = g.getColHint(i)
+		h.Rows[i] = g.getRowHint(i)
+	}
+	return h
+}
+
+func (g *Game) getColHint(col int) Hint {
+	m, p := 0, 0
+	for i := col; i < len(g.Board); i += 5 {
+		v := g.Board[i]
+		if v == 0 {
+			m++
+		}
+		p += int(v)
+	}
+	return Hint{m, p}
+}
+
+func (g *Game) getRowHint(row int) Hint {
+	m, p := 0, 0
+	for i := 5 * row; i < 5*row+5; i++ {
+		v := g.Board[i]
+		if v == 0 {
+			m++
+		}
+		p += int(v)
+	}
+	return Hint{m, p}
 }
--- model/game_test.go
+++ model/game_test.go
@@ -72,3 +72,126 @@ func TestGame_GetVisibleRevealed(t *testing.T) {
 		t.Error("expected", want, "got", got)
 	}
 }
+
+func TestGame_GetHintsBasic(t *testing.T) {
+	g := model.Game{ID: "1", Board: [25]byte{
+		1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1,
+	}}
+
+	want := model.Hints{
+		Rows: [5]model.Hint{
+			{0, 5}, {0, 5}, {0, 5}, {0, 5}, {0, 5},
+		},
+		Cols: [5]model.Hint{
+			{0, 5}, {0, 5}, {0, 5}, {0, 5}, {0, 5},
+		},
+	}
+	got := g.GetHints()
+
+	if want != got {
+		t.Error("expected", want, "got", got)
+	}
+}
+
+func TestGame_GetHintsBombs(t *testing.T) {
+	g := model.Game{ID: "1", Board: [25]byte{
+		0, 1, 1, 1, 1,
+		1, 0, 1, 1, 1,
+		0, 1, 0, 1, 1,
+		1, 0, 1, 1, 1,
+		1, 1, 0, 1, 1,
+	}}
+
+	want := model.Hints{
+		Rows: [5]model.Hint{
+			{1, 4}, {1, 4}, {2, 3}, {1, 4}, {1, 4},
+		},
+		Cols: [5]model.Hint{
+			{2, 3}, {2, 3}, {2, 3}, {0, 5}, {0, 5},
+		},
+	}
+	got := g.GetHints()
+
+	if want != got {
+		t.Error("expected", want, "got", got)
+	}
+}
+
+func TestGame_GetHintsPoints(t *testing.T) {
+	g := model.Game{ID: "1", Board: [25]byte{
+		0, 1, 1, 1, 2,
+		1, 0, 1, 3, 1,
+		0, 4, 0, 1, 3,
+		1, 0, 2, 1, 1,
+		2, 1, 0, 1, 1,
+	}}
+
+	want := model.Hints{
+		Rows: [5]model.Hint{
+			{1, 5}, {1, 6}, {2, 8}, {1, 5}, {1, 5},
+		},
+		Cols: [5]model.Hint{
+			{2, 4}, {2, 6}, {2, 4}, {0, 7}, {0, 8},
+		},
+	}
+	got := g.GetHints()
+
+	if want != got {
+		t.Error("expected", want, "got", got)
+	}
+}
+
+func TestGame_GetMaxScoreZero(t *testing.T) {
+	g := model.Game{ID: "1", Board: [25]byte{
+		0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0,
+	}}
+
+	want := 0
+	got := g.GetMaxScore()
+
+	if want != got {
+		t.Error("expected", want, "got", got)
+	}
+}
+
+func TestGame_GetMaxScoreOne(t *testing.T) {
+	g := model.Game{ID: "1", Board: [25]byte{
+		1, 0, 0, 0, 0,
+		0, 1, 0, 0, 0,
+		0, 0, 1, 0, 0,
+		0, 0, 0, 1, 0,
+		0, 0, 0, 0, 1,
+	}}
+
+	want := 1
+	got := g.GetMaxScore()
+
+	if want != got {
+		t.Error("expected", want, "got", got)
+	}
+}
+
+func TestGame_GetMaxScoreGeneral(t *testing.T) {
+	g := model.Game{ID: "1", Board: [25]byte{
+		1, 0, 0, 0, 0,
+		0, 2, 0, 4, 0,
+		0, 3, 1, 0, 0,
+		0, 0, 4, 1, 0,
+		0, 0, 0, 0, 1,
+	}}
+
+	want := 96
+	got := g.GetMaxScore()
+
+	if want != got {
+		t.Error("expected", want, "got", got)
+	}
+}
