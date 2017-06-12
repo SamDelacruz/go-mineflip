4db1aa7061c8fe55dd31e9b56ebdfac7e41c9dcc
Sam Delacruz <samdelacruz91@gmail.com>
2017-06-11 22:39:11 -0700

    Remove old board generation

--- model/board.go
+++ model/board.go
@@ -10,24 +10,6 @@ import (
 	"github.com/jmcvetta/randutil"
 )
 
-func GenBoard() [25]byte {
-	var b [25]byte
-
-	choices := []randutil.Choice{
-		{Item: 0, Weight: 6},
-		{Item: 1, Weight: 13},
-		{Item: 2, Weight: 4},
-		{Item: 3, Weight: 2},
-	}
-
-	for i := range b {
-		v, _ := randutil.WeightedChoice(choices)
-		b[i] = byte(v.Item.(int))
-	}
-
-	return b
-}
-
 func GenBoardForLevel(level int) [25]byte {
 	vals := genVals(level)
 	idxs := nRandomIndices(25)
